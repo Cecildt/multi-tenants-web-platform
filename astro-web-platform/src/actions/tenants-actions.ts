8df7a2214780a3093664d1413ab946cd8652f4e3
Cecil <cecildt@gmail.com>
2025-01-25 00:09:57 +0100

    Refactor tenant actions and forms for improved error handling and redirection

--- astro-web-platform/src/actions/tenants-actions.ts
+++ astro-web-platform/src/actions/tenants-actions.ts
@@ -1,76 +1,84 @@
-import { defineAction } from 'astro:actions';
-import { z } from 'astro:schema';
-import { nanoid } from 'nanoid';
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { nanoid } from "nanoid";
 // import { db, Comment } from 'astro:db';
 
 export const tenants = {
   addTenant: defineAction({
-    accept: 'form',
+    accept: "form",
     input: z.object({
-      business_name: z.string().min(3, 'Company name is required').trim(),
-      tenant_name: z.string().min(3, 'Tenant name is required').toLowerCase().trim(),
-      email: z.string().email('Valid email is required').trim(),
+      business_name: z.string().min(3, "Company name is required").trim(),
+      tenant_name: z
+        .string()
+        .min(3, "Tenant name is required")
+        .toLowerCase()
+        .trim(),
+      email: z.string().email("Valid email is required").trim(),
     }),
     handler: async ({ business_name, tenant_name, email }) => {
       const tenant_id = nanoid();
-        console.log(tenant_id, business_name, tenant_name, email);
-    //   const comment = await db
-    //     .insert(Comment)
-    //     .values({
-    //       postSlug,
-    //       name,
-    //       email,
-    //       message,
-    //       createdAt: new Date(),
-    //     })
-    //     .returning();
+      console.log(tenant_id, business_name, tenant_name, email);
+      //   const comment = await db
+      //     .insert(Comment)
+      //     .values({
+      //       postSlug,
+      //       name,
+      //       email,
+      //       message,
+      //       createdAt: new Date(),
+      //     })
+      //     .returning();
 
-    //   return comment[0];
+      return { tenant_id: tenant_id };
     },
   }),
   editTenant: defineAction({
-    accept: 'form',
+    accept: "form",
     input: z.object({
       tenant_id: z.string().trim(),
-      business_name: z.string().min(3, 'Company name is required').trim(),
-      tenant_name: z.string().min(3, 'Tenant name is required').toLowerCase().trim(),
-      email: z.string().email('Valid email is required').trim(),
+      business_name: z.string().min(3, "Company name is required").trim(),
+      tenant_name: z
+        .string()
+        .min(3, "Tenant name is required")
+        .toLowerCase()
+        .trim(),
+      email: z.string().email("Valid email is required").trim(),
     }),
     handler: async ({ tenant_id, business_name, tenant_name, email }) => {
-        console.log(tenant_id, business_name, tenant_name, email);
-    //   const comment = await db
-    //     .insert(Comment)
-    //     .values({
-    //       postSlug,
-    //       name,
-    //       email,
-    //       message,
-    //       createdAt: new Date(),
-    //     })
-    //     .returning();
+      console.log(tenant_id, business_name, tenant_name, email);
+      //   const comment = await db
+      //     .insert(Comment)
+      //     .values({
+      //       postSlug,
+      //       name,
+      //       email,
+      //       message,
+      //       createdAt: new Date(),
+      //     })
+      //     .returning();
 
-    //   return comment[0];
+      return { tenant_id: tenant_id };
     },
   }),
   deleteTenant: defineAction({
-    accept: 'form',
+    accept: "form",
     input: z.object({
       tenant_id: z.string().trim(),
     }),
     handler: async ({ tenant_id }) => {
-        console.log("Delete tenant: ", tenant_id);
-    //   const comment = await db
-    //     .insert(Comment)
-    //     .values({
-    //       postSlug,
-    //       name,
-    //       email,
-    //       message,
-    //       createdAt: new Date(),
-    //     })
-    //     .returning();
+      console.log("Delete tenant: ", tenant_id);
+      //   const comment = await db
+      //     .insert(Comment)
+      //     .values({
+      //       postSlug,
+      //       name,
+      //       email,
+      //       message,
+      //       createdAt: new Date(),
+      //     })
+      //     .returning();
 
-    //   return comment[0];
+      return { tenant_id: tenant_id };
     },
   }),
 };
