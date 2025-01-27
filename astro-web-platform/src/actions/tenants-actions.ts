ffb8102a935268600ac0f7e56696aae65a3f5288
cecil <cecildt@gmail.com>
2025-01-27 21:33:23 +0100

    Refactor tenants actions and TenantsGrid for improved code readability and maintainability

--- astro-web-platform/src/actions/tenants-actions.ts
+++ astro-web-platform/src/actions/tenants-actions.ts
@@ -99,7 +99,14 @@ export const tenants = {
       //     })
       //     .returning();
 
-      return { tenant_id: tenant_id, business_name: "Test", tenant_name: "test", email: "qwer@qwerty.com", created_at: new Date(), updated_at: new Date() };
+      return {
+        tenant_id: tenant_id,
+        business_name: "Test",
+        tenant_name: "test",
+        email: "qwer@qwerty.com",
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
     },
   }),
 };
